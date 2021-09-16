4e3442c6313f045dc70cefc441db63dc06a94b56
abednarz210 <86257908+abednarz210@users.noreply.github.com>
2021-09-16 16:33:17 -0500

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session, create_session
 from sqlalchemy import create_engine, func
-
+import pandas as pd
+import datetime as dt
+from datetime import date 
 from flask import Flask, jsonify
 
 
@@ -54,59 +56,92 @@ def precipitation():
     session = Session(engine)
 
 # Find the most recent date in the data set.
-recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
 
-# Design a query to retrieve the last 12 months of precipitation data and plot the results. 
-# Starting from the most recent data point in the database. 
-# Calculate the date one year from the last date in data set.
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+    # Starting from the most recent data point in the database. 
+    # Calculate the date one year from the last date in data set.
 
-query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-recent_date = dt.date(2017, 8, 23)
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    recent_date = dt.date(2017, 8, 23)
 
-# Perform a query to retrieve the data and precipitation scores
+    # Perform a query to retrieve the data and precipitation scores
 
-prcp_scores = (
-    session.query(measurement.date, measurement.prcp).filter(measurement.date >= query_date)
-    .order_by(measurement.date)
-    return jsonify(prcp_scores)
-    
+    prcp_scores = session.query(measurement.date, measurement.prcp).filter(measurement.date >= query_date).order_by(measurement.date).all()
     session.close()
+
+    #prcp = list(np.ravel(prcp_scores))
+    
+    prcp_out = []
+    for date, precip in prcp_scores:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["precipitation"] = precip
+    
+        prcp_out.append(prcp_dict)
+
+
+    return jsonify(prcp_out)
+    
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     # Create session 
     session = Session(engine)
-most_active = (
-    session.query(measurement.station, station.name,func.count(measurement.station)).\
-    filter(measurement.station == station.station).group_by(measurement.station)
-    .order_by(func.count(measurement.id).desc()).all()
-)
-station_id = most_active[0][0]
-station_id_most = most_active[0][2]
+    most_active = (
+        session.query(measurement.station, station.name,func.count(measurement.station)).\
+        filter(measurement.station == station.station).group_by(measurement.station)
+        .order_by(func.count(measurement.id).desc()).all()
+    )
+    station_id = most_active[0][0]
+    station_id_most = most_active[0][2]
 
-#calculate lowest, highest, avg temp
-most_active = ( 
-    session.query(
-    func.min(measurement.tobs),
-    func.max(measurement.tobs),
-    func.avg(measurement.tobs),
-)   
-.filter(measurement.station == most_active[0][0]).all()
-)
+    #calculate lowest, highest, avg temp
+    most_active = ( 
+        session.query(
+        func.min(measurement.tobs),
+        func.max(measurement.tobs),
+        func.avg(measurement.tobs),
+    )   
+    .filter(measurement.station == most_active[0][0]).all()
+    )
+    session.close()
 
     return jsonify(most_active)
-session.close()
+
 
 @app.route("/api/v1.0/stations")
+
 def stations():
-temp_obs = session.query(measurement.tobs).\
-    filter(measurement.station == station_id).filter(measurement.date <= query_date).all()
+    station_id = "USC00519281"
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    temp_obs = session.query(measurement.tobs).\
+        filter(measurement.station == station_id).filter(measurement.date <= query_date).all()
 
-temp_obs_df = pd.DataFrame(temp_obs, columns=['tobs'])
-temp_obs_df.head()
+    temp_obs_df = pd.DataFrame(temp_obs, columns=['tobs'])
+    temp_obs_df.head()
 
-    return jsonify(temp_obs)
     session.close()
+    temp = list(np.ravel(temp_obs))
+    return jsonify(temp)
+
+#######FIX THIS- keep this code!
+    # This function called `calc_temps` will accept start date and end date in the format '%Y-%m-%d' 
+# and return the minimum, maximum, and average temperatures for that range of dates
+def calc_temps(start_date, end_date):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
 
     
     #####@app.route("/api/v1.0/<start> and /api/v1.0/<start>/<end>")
